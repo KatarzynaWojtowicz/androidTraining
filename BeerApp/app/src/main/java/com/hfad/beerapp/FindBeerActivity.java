34f8beaf547a647c848e09c89dad481add7e42b3
Katarzyna Wojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-02-03 13:58:01 +0100

    Cleanup

--- BeerApp/app/src/main/java/com/hfad/beerapp/FindBeerActivity.java
+++ BeerApp/app/src/main/java/com/hfad/beerapp/FindBeerActivity.java
@@ -7,15 +7,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class FindBeerActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
     }
+
     public void onClickFindBeer(View view) {
         TextView brands = (TextView) findViewById (R.id.brands);
         Spinner color = (Spinner) findViewById(R.id.color);
         String beerType = String.valueOf(color.getSelectedItem());
-        brands. setText(beerType);
-        }
+        brands.setText(beerType);
+    }
 }
