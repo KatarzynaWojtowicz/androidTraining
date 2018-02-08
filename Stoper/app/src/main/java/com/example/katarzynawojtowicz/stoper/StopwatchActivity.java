45e265cc3976d619f5438ab7681a8456d8a7e07c
Katarzyna Wojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-02-08 21:05:40 +0100

    Renamed button

--- Stoper/app/src/main/java/com/example/katarzynawojtowicz/stoper/StopwatchActivity.java
+++ Stoper/app/src/main/java/com/example/katarzynawojtowicz/stoper/StopwatchActivity.java
@@ -1,13 +1,29 @@
 package com.example.katarzynawojtowicz.stoper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
 
-public class StopwatchActivity extends AppCompatActivity {
+public class StopwatchActivity extends Activity {
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
     }
 }
