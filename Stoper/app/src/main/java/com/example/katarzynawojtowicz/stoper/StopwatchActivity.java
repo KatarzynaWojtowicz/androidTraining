9aef83f2147e1bf8ae524e7e8035c1464ad1b861
Katarzyna Wojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-02-08 21:39:41 +0100

    Added methods

--- Stoper/app/src/main/java/com/example/katarzynawojtowicz/stoper/StopwatchActivity.java
+++ Stoper/app/src/main/java/com/example/katarzynawojtowicz/stoper/StopwatchActivity.java
@@ -5,25 +5,20 @@ import android.app.Activity;
 import android.view.View;
 
 public class StopwatchActivity extends Activity {
-    private int seconds = 0;
-    private boolean running;
-
+    private int seconds = 0; private boolean running;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
     }
 
-    public void onClickStart(View view) {
-        running = true;
+    public void onClickStart(View view) { running = true;
     }
 
-    public void onClickStop(View view) {
-        running = false;
+    public void onClickStop(View view) { running = false;
     }
 
-    public void onClickReset(View view) {
-        running = false;
+    public void onClickReset(View view) { running = false;
         seconds = 0;
     }
 }
